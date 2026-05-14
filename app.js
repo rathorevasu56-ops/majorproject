f784515fba383164ee7ce827fca5a4767e684eab
delta student <radharathore4983@gmail.com>
2026-05-14 17:19:13 +0530

    fixed cloudConfig filename

--- app.js
+++ app.js
@@ -71,36 +71,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // ================= SESSION STORE =================
 
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
-  crypto: {
-    secret: process.env.SECRET,
-  },
-  touchAfter: 24 * 3600,
-});
-
-store.on("error", (err) => {
-  console.log("SESSION STORE ERROR", err);
-});
 
 
 // ================= SESSION CONFIG =================
 
-const sessionOptions = {
-  store,
-  secret: process.env.SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: {
-    expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    httpOnly: true,
-  },
-};
 
-app.use(session(sessionOptions));
-
-app.use(flash());
 
 
 // ================= PASSPORT =================
